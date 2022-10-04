1dbe65be7a96bf3801e6643275ae40fc5908098d
KATAR5I5 <malex999@bk.ru>
2022-10-04 17:16:49 +0300

    комментарий перенесен на новую строку
    mainCalc

--- src/MainCalcularot.java
+++ src/MainCalcularot.java
@@ -2,6 +2,8 @@ public class MainCalcularot {
     public static void main(String[] arg) throws Exception {
 // объект ввода с клавиатуры
         InputKeyboard inputKeyboard = new InputKeyboard();
+
+
         // Массив операндов из Веденной с клавиатуры строки
         String[] inputArrayOperands = inputKeyboard.getArrayOperands();
         System.out.println(calc(inputArrayOperands));
