4f295a19e8f425067fd64ebf7bb813a6fc19ffd8
KATAR5I5 <malex999@bk.ru>
2022-10-04 17:51:25 +0300

    Change ENUM
    Удалил 2 параметр римских цифр, нет в этом параметре необходимости

--- src/RomeNumber.java
+++ src/RomeNumber.java
@@ -1,32 +1,32 @@
 public enum RomeNumber {
-    Zero(0,"0"),
-    I(1,"I"),
-    II(2,"II"),
-    III(3,"III"),
-    IV(4,"IV"),
-    V(5,"V"),
-    VI(6,"VI"),
-    VII(7,"VII"),
-    VIII(8,"VIII"),
-    IX(9,"IX"),
-    X(10,"X"),
-    XI(11,"XI"),
-    XII(12,"XII"),
-    XIII(13,"XIII"),
-    XIV(14,"XIV"),
-    XV(15,"XV"),
-    XVI(16,"XVI"),
-    XVII(17,"XVII"),
-    XVIII(18,"XVIII"),
-    XIX(19,"XIX"),
-    XX(20,"XX");
+    Zero(0),
+    I(1),
+    II(2),
+    III(3),
+    IV(4),
+    V(5),
+    VI(6),
+    VII(7),
+    VIII(8),
+    IX(9),
+    X(10),
+    XI(11),
+    XII(12),
+    XIII(13),
+    XIV(14),
+    XV(15),
+    XVI(16),
+    XVII(17),
+    XVIII(18),
+    XIX(19),
+    XX(20);
 
     private int num;
-    private String name;
-    RomeNumber(int num, String name){
+
+    RomeNumber(int num){
         this.num = num;
-        this.name = name;
     }
+
     public Integer getNumber(){
         return num;
     }
