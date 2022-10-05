4d608613758b41f8845c32e01c64fa1cda272976
KATAR5I5 <malex999@bk.ru>
2022-10-05 16:19:56 +0300

    Change switch case
    переделал switch -case  в методе ArabicCalculate

--- src/ArabicCalculate.java
+++ src/ArabicCalculate.java
@@ -2,35 +2,20 @@ public class ArabicCalculate {
     public String result;
 
     ArabicCalculate(int a, int b, String sign) throws Exception {
-//        String sign = c;
         if (a >= 0 && b >= 0) {
-            switch (sign) {
-                case "+":
-                    result = String.valueOf(a + b);
-                    break;
-                case "-":
-                    int subtraction = a - b;
-                    result = Integer.toString(subtraction);
-                    break;
-                case "*":
-                    int multiplication = a * b;
-                    result = Integer.toString(multiplication);
-                    break;
-                case "/":
-                    if (b != 0) {
-                        double division = a / b;
-
-                        result = Double.toString(division);
-
-                    } else result = "Ошибка! Деление на ноль!!!";
-                    break;
-                default:
+            result = switch (sign) {
+                case "+" -> String.valueOf(a + b);
+                case "-" -> Integer.toString(a - b);
+                case "*" -> Integer.toString(a * b);
+                case "/" -> (b != 0) ? Integer.toString(a / b) : "Ошибка! Деление на ноль!!!";
+                default -> {
                     try {
                         throw new Exception();
                     } catch (Exception e) {
-                        result = "throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)";
+                        yield "throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)";
                     }
-            }
+                }
+            };
         } else {
             try {
                 throw new Exception();
