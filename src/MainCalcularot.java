fd975f5790fef4fc4c5be3766ab2e03251c0b996
KATAR5I5 <malex999@bk.ru>
2022-10-07 11:37:07 +0300

    Добавил подсчет времени

--- src/MainCalcularot.java
+++ src/MainCalcularot.java
@@ -1,11 +1,16 @@
+import java.util.Date;
+
 public class MainCalcularot {
     public static void main(String[] arg)  {
 //        объект ввода с клавиатуры
         InputKeyboard inputKeyboard = new InputKeyboard();
 //        Массив операндов из Веденной с клавиатуры строки
+        Date start = new Date();
         String[] inputArrayOperands = inputKeyboard.getArrayOperands();
 
         MathClass math = new MathClass();
         System.out.println(math.calc(inputArrayOperands));
+        Date stop = new Date();
+        System.out.println((stop.getTime() - start.getTime()) + " ms");
     }
 }
